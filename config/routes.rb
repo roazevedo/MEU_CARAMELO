12a8aefb00f3bb7401e6dae883c439b470deef88
fernando chaim <fmchaim@gmail.com>
2024-02-24 20:18:31 -0300

    controllers testimony e adoption com before action e view animals e rotas

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  patch "adoptions/:id/update_status", to: "adoptions#update_status", as: :update_status
+  patch "adoptions/:id/update_done", to: "adoptions#done", as: :done
+
   resources :animals
+  resources :users do
+    resources :animals do
+      resources :adoptions, only: [:index, :show, :new, :create, :edit, :update] do
+        resources :testimonies, only: [:index, :new, :create, :show, :edit, :update]
+      end
+    end
+  end
 end
