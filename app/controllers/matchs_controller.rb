5755e92842214ac552b0e1379278e0cf39b19a64
marcosaccarvalho <informatica@bikraft.com.br>
2024-03-07 19:17:41 -0300

    match_dash

--- app/controllers/matchs_controller.rb
+++ app/controllers/matchs_controller.rb
@@ -3,28 +3,20 @@ class MatchsController < ApplicationController
 
   def show
     @user = User.find_by(id: current_user)
-    # @animals = Animal.all
-    # @matches = @animals.map do |animal|
-    #   score = match(@user, animal)
-    #   #tipo = match2(@user, animal)
-    #   { animal: animal, score: score} if score > 0
-    # end.compact
-
     @animals = Animal.all
     @matches = []
 
     @animals.each do |animal|
       match_data = match(@user, animal)
-      if match_data[:score] > 0
+      if match_data && match_data[:score] > 0
         @matches << { animal: animal, match_data: match_data }
       end
     end
-
-
   end
 
 
   def match(user, animal)
+    if user.specie == animal.specie
     match_data = {
       size_match: user.size == animal.size,
       specie_match: user.specie == animal.specie,
@@ -33,11 +25,9 @@ class MatchsController < ApplicationController
       vaccination_match: user.vaccination == animal.vaccination,
       neutered_match: user.neutered == animal.neutered
     }
-
     match_data[:score] = match_data.values.count(true)
+
+    end
     match_data
   end
-
-
-
 end
