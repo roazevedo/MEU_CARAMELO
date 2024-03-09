6f5398c12554ba7b9e017cca679d45b2b80edda2
fernando chaim <fmchaim@gmail.com>
2024-03-09 15:01:04 -0300

    rotas absurdas by Ed

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :animals
-  has_one :adoption_forms
+  has_one :adoption_form
   has_many :adoptions
   has_one_attached :photo
 end
